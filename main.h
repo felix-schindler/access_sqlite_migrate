a3aeef409c27e424d5a572f8e6802d2a9263689d
Felix <felix-schindler@outlook.com>
2023-07-16 01:08:13 +0200

    Initial commit

---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by felix on 16.07.2023.
+//
+
+#ifndef ACCESS_SQLITE_MIGRATE_MAIN_H
+#define ACCESS_SQLITE_MIGRATE_MAIN_H
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#endif //ACCESS_SQLITE_MIGRATE_MAIN_H
